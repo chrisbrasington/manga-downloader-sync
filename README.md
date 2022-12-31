c61c854e0ac385568093ff7904fec175622ade3c
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2022-12-31 12:11:01 -0700

    device detection

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # manga-downloader
+
+```
+python program.py                                                                           
+
+✓ kobo detected
+The Overworked Office Lady's Café Crush - https://danke.moe/read/manga/OL-cafe-crush/
+The Overworked Office Lady's Café Crush - Chapter 3 https://danke.moe/read/manga/OL-cafe-crush/3/1/
+✓ OL-cafe-crush-3.cbz
+✓ device sync
+The Overworked Office Lady's Café Crush - Chapter 2 https://danke.moe/read/manga/OL-cafe-crush/2/1/
+OL-cafe-crush-2.cbz: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 15.5M/15.5M [00:00<00:00, 545MB/s]
+✓ OL-cafe-crush-2.cbz
+✓ device sync
+The Overworked Office Lady's Café Crush - Chapter 1 https://danke.moe/read/manga/OL-cafe-crush/1/1/
+OL-cafe-crush-1.cbz: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 3.11M/3.11M [00:00<00:00, 143MB/s]
+✓ OL-cafe-crush-1.cbz
+✓ device sync
+```
--- program.py
+++ program.py
@@ -13,6 +13,11 @@ sources = [source.strip() for source in sources]
 
 util = Utility()
 
+if not os.access(sync_destination, os.W_OK):
+    print('kobo not plugged in')
+else:
+    print('✓ kobo detected')
+
 # Iterate over the list of sources
 for source in sources:
     # Parse the RSS feed
@@ -52,10 +57,12 @@ for source in sources:
                             # Update the progress bar manually
                             t.update(len(chunk))
 
-            sync_dest_file = os.path.join(feed.feed.title, filename)
-            
-            if not os.path.exists(sync_dest_file):
-                os.makedirs(os.path.dirname(sync_dest_file), exist_ok=True)
-                shutil.copy(filepath, sync_dest_file)
+            sync_dest_file = os.path.join(sync_destination, feed.feed.title, filename)
 
-            print('✓ ', name)
+            print('✓', name)
+
+            if os.access(sync_destination, os.W_OK):
+                if not os.path.exists(sync_dest_file):
+                    os.makedirs(os.path.dirname(sync_dest_file), exist_ok=True)
+                    shutil.copy(filepath, sync_dest_file)
+                print('✓ device sync')

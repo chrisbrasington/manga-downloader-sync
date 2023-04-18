9fe17e41d66c8b982f8b0d6542f38d969b8dc27e
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2023-04-17 20:15:31 -0600

    more sensible args

--- program.py
+++ program.py
@@ -11,19 +11,41 @@ sync_destination = f'{device}/manga'
 def main(args):
 
     current_time = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
-    print('Current time: ', current_time, end='')
+    print('Current time: ', current_time)
 
     # parsing utility
     util = Utility()
 
+    quit_early = False
+
+    # if add url to sources
+    if args.add is not None:
+        print('adding: ', args.add)
+        util.add_url_to_file(args.add, SourceFile.SOURCES.value)
+        quit_early = True
+    
+    # if completed add to completed
+    if args.completed is not None:
+        print('adding: ', args.completed)
+        util.add_url_to_file(args.completed, SourceFile.COMPLETED.value)
+        quit_early = True
+
+    # if hiatus add to hiatus
+    if args.hiatus is not None:
+        print('adding: ', args.hiatus)
+        util.add_url_to_file(args.hiatus, SourceFile.HIATUS.value)
+        quit_early = True
+    
+    # quit early if add url
+    if quit_early:
+        print('Done')
+        return
+
     # if url provided
     if args.url is not None:
-        # if adding to sources for future runs
-        if args.add:
-            util.add_url_to_file(args.url, args.add)
-
         # process url
         util.process_collection(args.url, sync_destination)
+        return
     
     # if cbz file to convert to pdf
     if args.file is not None:
@@ -37,45 +59,30 @@ def main(args):
             print(f'Done: {args.file}')
 
     else:
-    
-        # if no param run or ongoing
-        if len(sys.argv) == 1 or args.ongoing:
-            util.process_collection(util.get_collection(args.ongoing), sync_destination)
 
         # if completed run
-        if args.completed:
+        if args.completed is not None:
             util.process_collection(util.get_collection(args.completed), sync_destination)
         
-        # if haitus run
-        if args.haitus:
-            util.process_collection(util.get_collection(args.haitus), sync_destination)
+        # if hiatus run
+        elif args.hiatus is not None:
+            util.process_collection(util.get_collection(args.hiatus), sync_destination)
+        # if normal run
+        else:
+            util.process_collection(util.get_collection(SourceFile.SOURCES.value), sync_destination)
 
     # print summary 
     util.print_summary()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Manga-Kobo downloader and sync', add_help=True)
-    parser.add_argument('-u', '--url', help='Url to read from', required=False)
+    parser.add_argument('-u', '--url', help='Url to read from', required=False, nargs='?')
     parser.add_argument('-a', '--add', help='Add url to sources.txt', required=False, nargs='?')
-    parser.add_argument('-o', '--ongoing', help='Use ongoing sources.txt', required=False, nargs='?')
     parser.add_argument('-c', '--completed', help='Use completed.txt', required=False, nargs='?')
-    parser.add_argument('-d', '--haitus', help='Use dead haitus.txt', required=False, nargs='?')
+    parser.add_argument('-d', '--hiatus', help='Use dead hiatus.txt', required=False, nargs='?')
     parser.add_argument('-f', '--file', help='Convert an existing cbz file to pdf', required=False)
     args = parser.parse_args()
 
-    # this is a bit dumb but I can't tell the difference between
-    # and args with None vs its absense
-    # so I check for the argv and populate it so that
-    #    "if args.ongoing" is easier to discern later
-    # plus it's easier to match commands to the files in use anyway
-    if '-a' in sys.argv or '--add' in sys.argv:
-        args.add = SourceFile.SOURCES.value
-    if '-o' in sys.argv or '--ongoing' in sys.argv or len(sys.argv) == 1:
-        args.ongoing = SourceFile.SOURCES.value
-    if '-c' in sys.argv or '--completed' in sys.argv:
-        args.ongoing = SourceFile.COMPLETED.value
-    if '-d' in sys.argv or '--haitus' in sys.argv:
-        args.ongoing = SourceFile.HIATUS.value
-    # -f requires a value
+    # print(args)
 
     main(args)
